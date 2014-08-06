47161164aa8e528a463cffa1bfe6cd404fdbc38b
jcj <juanc.jara@ucp.pe>
2014-08-06 17:50:35 -0500

    algorithm

--- gg/js/logic.js
+++ gg/js/logic.js
@@ -126,47 +126,46 @@ var logic = {
             minVal = { lower:21 , upper : 21 },
             minWeight=  { lower : 0 , upper : 0 },
             weightBound = { lower : 0 , upper : 0 } ;
-        if ( sons.length == 0 && !eva.getLocked() ){
+        if (!sons.length && !eva.getLocked()){
             return { lower: 0 , upper : 20 };
         }
-        if ( sons.length == 0 ){
+        if (!sons.length){
             return eva.getBounds();
         }
-        no es necesario
-        if ( sons.length > 0 ){
-            for ( var i = 0 ; i < sons.length ; i++ ){
-                var temp = logic.simulation( sons[i] ),
-                    weight = sons[i].getWeight();
-                bounds.lower += temp.lower* weight;
-                bounds.upper += temp.upper* wei
-                weights += weight;
+        
+        for (var i = 0 ; i < sons.length ; i++) {
+            var temp = logic.simulation(sons[i]),
+                weight = sons[i].getWeight();
+            bounds.lower += temp.lower* weight;
+            bounds.upper += temp.upper* weight;
+            weights += weight;
 
-                if ( temp.lower < minVal.lower ){
-                    minVal.lower = temp.lower;
-                    minWeight.lower = weight;
-                }
+            if (temp.lower < minVal.lower){
+                minVal.lower = temp.lower;
+                minWeight.lower = weight;
+            }
 
-                if ( temp.upper < minVal.upper ){
-                    minVal.upper = temp.upper;
-                    minWeight.upper = weight;
-                }
+            if (temp.upper < minVal.upper){
+                minVal.upper = temp.upper;
+                minWeight.upper = weight;
             }
-            weightBound.lower = weights;
-            weightBound.upper = weights;
-            if ( eva.shouldDeleteMinimum() ){
-                bounds.lower -= minVal.lower;
-                bounds.upper -= minVal.upper;
-                weightBound.lower -= minWeight.lower;
-                weightBound.upper -= minWeight.upper;
-            }
-            bounds.lower /= weightBound.lower;
-            bounds.upper /= weightBound.upper;
-            var precision = eva.getPrecision();
-            bounds.lower = Math.floor( bounds.lower * precision ) / precision;
-            bounds.upper = Math.floor( bounds.upper * precision ) / precision;
-            eva.setBounds ( bounds );
-            return bounds;
         }
+        weightBound.lower = weights;
+        weightBound.upper = weights;
+        if (eva.shouldDeleteMinimum()){
+            bounds.lower -= minVal.lower;
+            bounds.upper -= minVal.upper;
+            weightBound.lower -= minWeight.lower;
+            weightBound.upper -= minWeight.upper;
+        }
+        bounds.lower /= weightBound.lower;
+        bounds.upper /= weightBound.upper;
+        var precision = eva.getPrecision();
+        bounds.lower = Math.floor(bounds.lower * precision) / precision;
+        bounds.upper = Math.floor(bounds.upper * precision) / precision;
+        eva.setBounds(bounds);
+        return bounds;
+        
     },
     execute : function( eva , testValue ){
         var sons = eva.getSons(),
