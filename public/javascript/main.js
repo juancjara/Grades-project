493bc264efeb8070389f05899d2e619e03f22577
jcj <juanc.jara@ucp.pe>
2014-08-15 10:31:48 -0500

    success messages added

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,7 +1,7 @@
 var main = {
-  init: function(api) {
+  init: function(api, strings) {
     var courseList = courseSearch(api);
-    var my_list = course(api);
+    var my_list = course(api, main, strings);
 
     var selected_handler = function(course, elem) {
       $('.active').removeClass('active');
@@ -16,8 +16,34 @@ var main = {
     courseList.add_handler = my_list.add;
     courseList.selected_handler = selected_handler;
     courseList.new_course_handler = my_list.create;
+  },
+  config: {
+    msg_template: {
+      part1: '<div class="alert ',
+      part2: '" role="alert"><button class="close" data-dismiss="alert">'+
+             '<span aria-hidden="true">&times<span class="sr-only">Close'+
+             '</span></span></button>',
+      part3: '</div>'
+    }
+  },
+  show_ok_msg: function(msg) {
+    main.show_msg(msg, 'alert-success');
+  },
+  show_err_msg: function(msg) {
+    main.show_msg(msg, 'alert-danger');
+  },
+  show_msg: function(msg, classParam) {
+    var init_template = main.config.msg_template;
+    var $template = $(init_template.part1 + classParam + init_template.part2 +
+        msg + init_template.part3);
+    $('#messages').append($template);
+    setTimeout(function() {
+      $template.fadeOut('slow', function() {
+        $template.remove();
+      });
+    },1000);
   }
 }
 $(function() {
-  main.init(api);
+  main.init(api, strings);
 });
---
+++ public/javascript/string.js
@@ -0,0 +1,7 @@
+var strings = {
+  course_create_ok : 'Curso creado.',
+  course_edit_ok: 'Información actualizada.',
+  course_remove_ok: 'Curso eliminado.',
+  course_share_ok: 'Curso compartido.',
+  course_error: 'GG feeder'
+};
